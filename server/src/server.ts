8afd25f542cab0cc28820a71472104676cdae066
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-01-17 06:29:59 -0300

    feat: WIP initial setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,21 @@
-console.log("hello worldd!!");
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
+
+const app = Fastify();
+const prisma = new PrismaClient();
+
+app.register(cors, {
+  origin: ["http://localhost:3000"],
+});
+
+app.get("/habits", async () => {
+  const habits = await prisma.habit.findMany();
+  return habits;
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => console.log("Server is running!"));
