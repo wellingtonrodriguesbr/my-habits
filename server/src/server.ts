681094556054606b26fe1538fe7de5ff91e4517b
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-01-19 06:50:48 -0300

    chore: organize files

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,16 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
+import { appRoutes } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
-app.register(cors, {
-  origin: ["http://localhost:3000"],
-});
+app.register(cors);
 
-app.get("/habits", async () => {
-  const habits = await prisma.habit.findMany();
-  return habits;
-});
+app.register(appRoutes);
 
 app
   .listen({
     port: 3333,
   })
-  .then(() => console.log("Server is running!"));
+  .then(() => console.log("Server is running!"))
+  .catch((e) => console.log(e));
