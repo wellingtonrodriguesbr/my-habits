6962468fa5045f45b7e5d2e0297e8a616d7fb707
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-01-20 05:56:05 -0300

    feat: WIP home page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,13 @@
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
+
 export function App() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-9xl font-bold">Hello World!</h1>
+    <div className="flex justify-center items-center h-screen w-screen">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
     </div>
   );
 }
