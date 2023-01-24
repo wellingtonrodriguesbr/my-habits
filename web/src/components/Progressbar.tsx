3f57721cd1231a67ecb7ef73da69ace44c10b7b5
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-01-24 06:07:29 -0300

    feat: create Progressbar component

---
+++ web/src/components/Progressbar.tsx
@@ -0,0 +1,17 @@
+interface ProgressbarProps {
+  progress: number;
+}
+
+export function Progressbar({ progress }: ProgressbarProps) {
+  return (
+    <div className="h-3 rounded-xl w-full bg-zinc-700 mt-4">
+      <div
+        role="progressbar"
+        aria-label="Progresso de hábitos completados nesse dia"
+        aria-aria-valuenow={progress}
+        className="h-3 rounded-xl bg-violet-500 transition-colors"
+        style={{ width: `${progress}%` }}
+      ></div>
+    </div>
+  );
+}
