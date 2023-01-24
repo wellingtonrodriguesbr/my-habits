a09bc3afb8456b6bbb04ac55130c22ce91069843
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-01-24 06:08:20 -0300

    chore: random completed habits

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -17,7 +17,11 @@ export function SummaryTable() {
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map((date) => (
-          <HabitDay key={date.toString()} />
+          <HabitDay
+            key={date.toString()}
+            amount={5}
+            completed={Math.random() * 5}
+          />
         ))}
         {amountOfDaysToFill > 0 &&
           Array.from({ length: amountOfDaysToFill }).map((_, index) => (
